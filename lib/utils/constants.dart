28cf08faf37e938dc2cac7e8f74bdb910e904416
adel4780 <adelk138019@gmail.com>
2023-06-04 11:13:44 +0330

    feat(develop) : add constants.dart and plugins

---
+++ lib/utils/constants.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+// Colors
+const Color WhiteColor = Color(0xFFFFFFFF);
+const Color BlackColor = Color(0xff000000);
+const Color LightBlueColor = Color(0xff1fdada);
+const Color DarkBlueColor = Color(0xff0080fc);
+const Color GreyColor = Color(0xff5d5d5d);
+
+// Pages
+String MaskGroupPage = "/maskgroupPage";
+String RulesPage = "/rulePage";
+String AboutPage = "/aboutPage";
+String ContactUsPage = "/contactUsPage";
+String HomePage = "/homePage";
+String SuccessfulPurchasePage = "/successfulPurchasePage";
+String UnSuccessfulPurchasePage = "/unSuccessfulPurchasePage";
+
+// Images
+
+// Fonts
+String IranSansWeb = "IranSansWeb";
+
