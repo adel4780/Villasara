2eb9506574eba2c1da9982a0b975cbc8a45ed2c7
adel4780 <adelk138019@gmail.com>
2023-06-04 11:49:45 +0330

    feat(develop) : add images from design

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -16,8 +16,27 @@ String HomePage = "/homePage";
 String SuccessfulPurchasePage = "/successfulPurchasePage";
 String UnSuccessfulPurchasePage = "/unSuccessfulPurchasePage";
 
+//Strings, Names, Address
+String AppName = "ویلا سرا";
+String AppNameEn = "VillaSara";
+String AppVersion = "";
+String Phone = "35269854 (031)";
+String Email = "villaSara@gmail.com";
+String Name = "";
+String Address = "اصفهان - میدان آزادی - دانشگاه اصفهان";
+
 // Images
+String Linkedin = "assets/images/linkedin.png";
+String Twitter = "assets/images/twitter.png";
+String Instagram = "assets/images/instagram.png";
+String Telegram = "assets/images/telegram-app.png";
+String Logo = "assets/images/logo.png";
+String ENamad = "assets/images/enamad.png";
+String Map = "assets/images/map.png";
+String Tick = "assets/images/Tick.png";
+String Zabdar = "assets/images/zabdar.png";
 
 // Fonts
 String IranSansWeb = "IranSansWeb";
+String FugazOne = "FugazOne";
 
