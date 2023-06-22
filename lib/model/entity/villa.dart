38afc4521af88fd7b8e455cac02a06e1152556bc
adel4780 <adelk138019@gmail.com>
2023-06-22 18:33:48 +0330

    feat(feature/home-page) : add imageList

--- lib/model/entity/villa.dart
+++ lib/model/entity/villa.dart
@@ -1,24 +1,31 @@
+import 'image.dart';
+
 class Villa{
   int? id;
+  int? villaOwner;
   String? state;
   String? city;
   String? region;
   String? address;
   String? description;
   String? pricePerNight;
+  List<VillaImage>? images;
 
   Villa({
     this.id,
+    this.villaOwner,
     this.state,
     this.city,
     this.region,
     this.address,
     this.description,
     this.pricePerNight,
+    this.images,
 });
   factory Villa.fromJson(Map<String, dynamic> json) {
     return Villa(
       id: json['id'],
+      villaOwner: json['villa_owner'],
       state: json['state'],
       city: json['city'],
       region: json['region'],
@@ -30,6 +37,7 @@ class Villa{
   }
   Map<String, dynamic> toJson() => {
     'id' : id,
+    'villa_owner' : villaOwner,
     'state': state,
     'city': city,
     'region': region,
